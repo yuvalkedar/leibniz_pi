b337fdf496e3abec46249135210e83e83a621332
KD Tech <34989581+yuvalkedar@users.noreply.github.com>
2021-03-24 15:21:37 +0200

    Fixed PI print - more # after the decimal point

--- leibniz_pi.cpp
+++ leibniz_pi.cpp
@@ -1,12 +1,14 @@
 // Happy Pi day (March 14th) - too shame the year isn't 1592
 // Calculating Pi using Leibniz's formula
 // https://en.wikipedia.org/wiki/Leibniz_formula_for_π
+// Yuval Kedar - KD Technology
+// www.kdtech.co.il
 
 #include <Arduino.h>
 
 #define SERIAL_BAUDRATE	115200
 #define PI				3.14159265359
-#define FACTOR			10	// to see more clearly on the Serial Plotter
+#define FACTOR			1	// to see more clearly on the Serial Plotter
 #define REFRESH_MS		10
 
 float pi = 4;	// because in Leibniz's formula the series converges to Pi/4, so it's like saying 4 - 4/3 + 4/5 - 4/7 ... = Pi
@@ -29,7 +31,7 @@ void loop()
 
 	Serial.print(pi * FACTOR, 10);
 	Serial.print(" ~= ");
-	Serial.println(PI * FACTOR);
+	Serial.println(PI * FACTOR, 10);
 	delay(REFRESH_MS);
 	iterations++;
 }
